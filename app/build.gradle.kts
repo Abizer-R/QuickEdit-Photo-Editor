10a332dee1ed4e2f1347ba01d384aae8f7f46e49
rabiz <rabizeridris@gmail.com>
2024-11-26 20:11:17 +0530

    version update - 3 (1.0.3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.abizer_r.quickedit"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
