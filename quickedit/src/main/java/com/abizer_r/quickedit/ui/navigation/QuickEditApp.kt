9788186611491233bd102a42b3fe916dd7608303
rabiz <rabizeridris@gmail.com>
2025-08-28 13:01:01 +0530

    Apply the system bars padding for whole app (centralized padding, might need to change if we want a screen (composable) to stretch to full-screen in future)

--- quickedit/src/main/java/com/abizer_r/quickedit/ui/navigation/QuickEditApp.kt
+++ quickedit/src/main/java/com/abizer_r/quickedit/ui/navigation/QuickEditApp.kt
@@ -3,7 +3,12 @@ package com.abizer_r.quickedit.ui.navigation
 import android.net.Uri
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.abizer_r.quickedit.theme.QuickEditTheme
@@ -13,11 +18,18 @@ fun QuickEditApp(
     initialImageUri: Uri? = null
 ) {
     QuickEditTheme {
-        Box(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.background)
-        ) {
-            QuickEditNavigation(initialImageUri)
+        Scaffold(
+            // Ensure content avoids system bars & cutouts
+            contentWindowInsets = WindowInsets.safeDrawing,
+        ) { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .background(MaterialTheme.colorScheme.background)
+            ) {
+                QuickEditNavigation(initialImageUri)
+            }
         }
     }
 }
