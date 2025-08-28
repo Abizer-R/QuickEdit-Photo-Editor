1161ded0d63af1e0b77dd980fbb6017a8a2fd738
rabiz <rabizeridris@gmail.com>
2025-08-28 12:09:45 +0530

    Bump to api 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.abizer_r.quickedit"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.abizer_r.quickedit"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 5
         versionName = "1.1.1"
 
