1ac6e1a0d380ef20f830a3429557ae2d5f8228e1
rabiz <rabizeridris@gmail.com>
2025-08-28 13:00:15 +0530

    Enforce edge to edge for older APIs

--- app/src/main/java/com/abizer_r/quickedit/ui/main/MainActivity.kt
+++ app/src/main/java/com/abizer_r/quickedit/ui/main/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
 import com.abizer_r.quickedit.ui.navigation.QuickEditApp
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,6 +15,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         val imageUri: Uri? = extractImageUriFromIntent(intent)
 
