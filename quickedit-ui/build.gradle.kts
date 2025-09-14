5f03a832d10e5e6992a668b3643448c81baa2509
rabiz <rabizeridris@gmail.com>
2025-09-14 12:32:51 +0530

    fix dependency resolution issue for test classes in quickedit-ui module

--- quickedit-ui/build.gradle.kts
+++ quickedit-ui/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
 //    implementation(libs.androidx.core.ktx)
 //    implementation(libs.androidx.appcompat)
 //    implementation(libs.material)
-//    testImplementation(libs.junit)
-//    androidTestImplementation(libs.androidx.junit)
-//    androidTestImplementation(libs.androidx.espresso.core)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
